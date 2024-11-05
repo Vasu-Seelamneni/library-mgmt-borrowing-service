a965944dad9a8411d605b59223abede7d416997d
Vasu Seelamneni <Vasu_Seelamneni@Dell.com>
2024-11-05 15:58:40 +0800

    Library management borrowing services project

---
+++ src/main/java/com/msp/library/mgmt/librarymgmtborrowingservice/LibraryMgmtBorrowingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtborrowingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtBorrowingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMgmtBorrowingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msp/library/mgmt/librarymgmtborrowingservice/LibraryMgmtBorrowingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtborrowingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtBorrowingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
